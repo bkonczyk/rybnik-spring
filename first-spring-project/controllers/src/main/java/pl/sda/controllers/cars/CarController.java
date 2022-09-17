fd0216d89420f2c90facfa35e1809d9fcf600415
bkonczyk <bartkon@gmail.com>
2022-09-17 11:50:03 +0200

    basic controller all methods

--- first-spring-project/controllers/src/main/java/pl/sda/controllers/cars/CarController.java
+++ first-spring-project/controllers/src/main/java/pl/sda/controllers/cars/CarController.java
@@ -2,9 +2,7 @@ package pl.sda.controllers.cars;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,35 @@ public class CarController {
     List<Car> getAllCars() {
         log.info("Received a request to get all cars");
         return carService.getCars();
+    }
+
+    @GetMapping("/{make}")
+    List<Car> getAllCarsByMake(@PathVariable String make) {
+        log.info("Received request to get {} cars", make);
+        return carService.getCarsByMake(make);
+    }
+
+    @PostMapping
+    void addCar(@RequestBody Car car) {
+        log.info("Received request to add car {}", car);
+        carService.addCar(car);
+    }
+
+    @PostMapping("/list")
+    void addCars(@RequestBody List<Car> cars) {
+        log.info("Received request to add list car {}", cars);
+        carService.addCars(cars);
+    }
+
+    @PutMapping
+    void modifyFirstCar(@RequestBody Car car) {
+        log.info("Received request to modify first car {}", car);
+        carService.modifyFirst(car);
+    }
+
+    @DeleteMapping("/{index}")
+    void deleteAtIndex(@PathVariable Integer index) {
+        log.info("Received request to remove car at index {}", index);
+        carService.removeAtIndex(index);
     }
 }
--- first-spring-project/controllers/src/main/java/pl/sda/controllers/cars/CarService.java
+++ first-spring-project/controllers/src/main/java/pl/sda/controllers/cars/CarService.java
@@ -6,6 +6,8 @@ import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 @Service
 public class CarService {
 
@@ -15,6 +17,20 @@ public class CarService {
         return cars;
     }
 
+    public List<Car> getCarsByMake(String make) {
+        return cars.stream()
+                .filter(car -> make.equalsIgnoreCase(car.getMake()))
+                .collect(toList());
+    }
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public void addCars(List<Car> cars) {
+        this.cars.addAll(cars);
+    }
+
     @PostConstruct
     private void initializeCarsList() {
         cars.add(Car.of("Hyundai", 2020));
@@ -22,5 +38,14 @@ public class CarService {
         cars.add(Car.of("Lanos", 2001));
         cars.add(Car.of("Golf", 1988));
         cars.add(Car.of("Passeratti", 2015));
+        cars.add(Car.of(null, 2015));
+    }
+
+    public void modifyFirst(Car car) {
+        cars.set(0, car);
+    }
+
+    public void removeAtIndex(int index) {
+        cars.remove(index);
     }
 }
